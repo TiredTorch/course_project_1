1bec55e7c7dd980088821e003089b64f587928d0
Tr.Torch <vitochek337@gmail.com>
2022-02-02 14:17:13 +0200

    Homework 2.2: hotfix

--- Homework/2.3/transformStateWithClones/transformStateWithClones.js
+++ Homework/2.3/transformStateWithClones/transformStateWithClones.js
@@ -1,5 +1,7 @@
 export const transformStateWithClones = (state, transforms) => {
 
+  const cloneOfState = {...state};
+
   const intervalForms = [];
 
   transforms.forEach(element => {
@@ -8,26 +10,26 @@ export const transformStateWithClones = (state, transforms) => {
       case 'addProperties':
     
         for (const key in element.properties) {
-          state[key] = element.properties[key];
+          cloneOfState[key] = element.properties[key];
         }
 
-        intervalForms.push({...state});
+        intervalForms.push({...cloneOfState});
         break;
     
       case 'clear':
-        for (const key in state) {
-          delete state[key];
+        for (const key in cloneOfState) {
+          delete cloneOfState[key];
         }
 
-        intervalForms.push({...state});
+        intervalForms.push({...cloneOfState});
         break;
     
       case 'removeProperties':
         element.properties.forEach(prop => {
-          delete state[prop];
+          delete cloneOfState[prop];
         });
     
-        intervalForms.push({...state});
+        intervalForms.push({...cloneOfState});
         break;
         
       default:
