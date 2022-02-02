4519ce2f6a302000cd2a90d5de50bb48140b7b3d
Ivanov Victor <69712962+TiredTorch@users.noreply.github.com>
2022-02-02 13:41:35 +0200

    Update moveZeros.js

--- Homework/2.3/moveZeros.js
+++ Homework/2.3/moveZeros.js
@@ -1,15 +1,13 @@
 export const moveZeros = (arrayOfData) => {
 
-  const arrayWithoutZeros = arrayOfData.filter(element => element !== 0);
-  const amountOfZeros = arrayOfData.length - arrayWithoutZeros.length;
-
-  const arrayWithZeros = arrayWithoutZeros;
+  const resultedArray = arrayOfData.filter(element => element !== 0);
+  const amountOfZeros = arrayOfData.length - resultedArray.length;
 
   for (let index = 0; index < amountOfZeros; index++) {
     
-    arrayWithZeros.push(0);
+    resultedArray.push(0);
   }
 
-  return arrayWithZeros;
+  return resultedArray;
 
 };
