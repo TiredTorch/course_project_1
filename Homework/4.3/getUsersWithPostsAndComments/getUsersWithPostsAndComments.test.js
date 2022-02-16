26f721f34a12e212dfac25b5e5e8f0676b475447
Tr.Torch <vitochek337@gmail.com>
2022-02-16 10:20:51 +0200

    Created getUsersWithPostsAndComments test

--- Homework/4.3/getUsersWithPostsAndComments/getUsersWithPostsAndComments.test.js
+++ Homework/4.3/getUsersWithPostsAndComments/getUsersWithPostsAndComments.test.js
@@ -1,27 +1,86 @@
-/* eslint-disable no-console */
 import { getUsersWithPostsAndComments } from './getUsersWithPostsAndComments';
+import axios from 'axios';
 import mockAxios from 'axios';
 
-jest.mock('axios', () => ({
-  __esModule: true,
-  default: {
-    get: jest.fn().mockResolvedValue({ data: 'kkk' }),
-  },
-}));
+jest.mock('axios');
+afterEach(jest.clearAllMocks);
 
 describe('Describe getUsersWithPostsAndComments', () => {
 
-  afterEach(jest.clearAllMocks);
+  it('Should reject getUsersWithPostsAndComments', async() => {
+    axios.get.mockImplementationOnce(() => Promise.reject(null));
 
-  it('gpl3', async() => {
+    const result = await getUsersWithPostsAndComments();
     
+    expect(result).toStrictEqual(new Error);
+  });
+
+  it('Should resolve getUsersWithPostsAndComments', async() => {
+    
+    axios.get
+      .mockImplementationOnce(() => 
+        Promise.resolve(
+          { data: [
+            {
+              id: 1, email: 'kolya@gmail.com',
+            },
+          ] },
+        ))
+      .mockImplementationOnce(() => 
+        Promise.resolve(
+          { data: [
+            {
+              userId: 1, id: 1,
+            },
+          ] },
+        ))
+      .mockImplementationOnce(() => 
+        Promise.resolve(
+          { data: [
+            {
+              postId: 1, name: 'Mary',
+            },
+          ] },
+        ));
+
     const result = await getUsersWithPostsAndComments();
 
-    expect(result).toBe('kkk');
-    expect(mockAxios.get).toHaveBeenCalledTimes(1);
-    expect(mockAxios.get).toHaveBeenCalledWith('https://fakestoreapi.com/products');
+    expect(result).toStrictEqual([
+      {
+        id: 1,
+        email: 'kolya@gmail.com',
+        posts: [
+          {
+            userId: 1, id: 1, comments: [
+              {
+                postId: 1, name: 'Mary',
+              },
+            ],
+          },
+        ],
+      },
+    ]);
+
   });
-  
+
+  it('Should be called with proper url', async() => {
+    
+    axios.get.mockImplementationOnce(() => Promise.resolve({data: {}}));
+
+    await getUsersWithPostsAndComments();
+
+    expect(mockAxios.get).toHaveBeenCalledWith(
+      'https://jsonplaceholder.typicode.com/users',
+    );
+
+    expect(mockAxios.get).toHaveBeenCalledWith(
+      'https://jsonplaceholder.typicode.com/posts',
+    );
+
+    expect(mockAxios.get).toHaveBeenCalledWith(
+      'https://jsonplaceholder.typicode.com/comments',
+    );
+  });
   
 });
 
