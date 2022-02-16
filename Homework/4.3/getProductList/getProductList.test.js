76574be95f9d056cff860e36ca060afe68b028aa
Tr.Torch <vitochek337@gmail.com>
2022-02-16 10:20:10 +0200

    Created getProductList test

--- Homework/4.3/getProductList/getProductList.test.js
+++ Homework/4.3/getProductList/getProductList.test.js
@@ -1,24 +1,55 @@
-/* eslint-disable no-console */
 import { getProductList } from './getProductList';
+import axios from 'axios';
 import mockAxios from 'axios';
 
-jest.mock('axios', () => ({
-  __esModule: true,
-  default: {
-    get: jest.fn().mockResolvedValue({ data: 'aaa' }),
-  },
-}));
+jest.mock('axios');
+afterEach(jest.clearAllMocks);
 
 describe('Describe getProductList', () => {
 
-  afterEach(jest.clearAllMocks);
+  it('Should reject getProductList', async() => {
+    axios.get.mockImplementationOnce(() => Promise.reject(null));
 
-  it('gpl1', async() => {
+    const result = await getProductList();
     
+    expect(result).toStrictEqual(new Error);
+  });
+
+  it('Should resolve getProductList', async() => {
+    
+    axios.get.mockImplementationOnce(() => 
+      Promise.resolve(
+        { data: [
+          {
+            price: 10, category: 'w', 
+          }, {
+            price: 20, category: 'a', 
+          }, {
+            price: 5, category: 'a', 
+          },
+        ] },
+      ));
+
     const result = await getProductList();
 
-    expect(result).toBe('aaa');
-    expect(mockAxios.get).toHaveBeenCalledTimes(1);
+    expect(result).toStrictEqual([
+      {
+        price: 20, category: 'a',
+      }, {
+        price: 5, category: 'a',
+      }, {
+        price: 10, category: 'w',
+      },
+    ]);
+
+  });
+
+  it('Should be called with proper url', async() => {
+    
+    axios.get.mockImplementationOnce(() => Promise.resolve({data: {}}));
+
+    await getProductList();
+
     expect(mockAxios.get).toHaveBeenCalledWith('https://fakestoreapi.com/products');
   });
   
